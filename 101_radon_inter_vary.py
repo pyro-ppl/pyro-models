1ce92f5307fd1a535304ea7664c3545fe33fd252
jpchen <jonathanp.chen@gmail.com>
2019-01-04 16:55:50 -0800

    doing these one at a time boys

--- 101_radon_inter_vary.py
+++ 101_radon_inter_vary.py
@@ -6,8 +6,6 @@ import pyro.distributions as dist
 def init_vector(name, dims=None):
     return pyro.sample(name, dist.Normal(torch.zeros(dims), 0.2 * torch.ones(dims)))
 
-
-
 def validate_data_def(data):
     assert 'N' in data, 'variable not found in data: key=N'
     assert 'county' in data, 'variable not found in data: key=county'
@@ -21,70 +19,37 @@ def validate_data_def(data):
     x = data["x"]
     y = data["y"]
 
-def transformed_data(data):
-    # initialize data
-    N = data["N"]
-    county = data["county"]
-    u = data["u"]
-    x = data["x"]
-    y = data["y"]
-    inter = init_vector("inter", dims=(N)) # vector
-    inter = _pyro_assign(inter, _call_func("elt_multiply", [u,x]))
-    data["inter"] = inter
-
-def init_params(data, params):
-    # initialize data
-    N = data["N"]
-    county = data["county"]
-    u = data["u"]
-    x = data["x"]
-    y = data["y"]
-    # initialize transformed data
-    inter = data["inter"]
+def init_params(data):
+    params = {}
     # assign init values for parameters
-    params["a"] = init_vector("a", dims=(85)) # vector
-    params["b"] = init_vector("b", dims=(85)) # vector
-    params["beta"] = init_vector("beta", dims=(2)) # vector
-    params["mu_a"] = pyro.sample("mu_a"))
-    params["mu_b"] = pyro.sample("mu_b"))
-    params["mu_beta"] = pyro.sample("mu_beta"))
     params["sigma_a"] = pyro.sample("sigma_a", dist.Uniform(0., 100.))
     params["sigma_b"] = pyro.sample("sigma_b", dist.Uniform(0., 100.))
     params["sigma_beta"] = pyro.sample("sigma_beta", dist.Uniform(0., 100.))
     params["sigma_y"] = pyro.sample("sigma_y", dist.Uniform(0., 100.))
+    return params
 
 def model(data, params):
     # initialize data
     N = data["N"]
-    county = data["county"]
+    county = data["county"].long() - 1
     u = data["u"]
     x = data["x"]
     y = data["y"]
     # initialize transformed data
-    inter = data["inter"]
-    
+    inter = u * x
+
     # init parameters
-    a = params["a"]
-    b = params["b"]
-    beta = params["beta"]
-    mu_a = params["mu_a"]
-    mu_b = params["mu_b"]
-    mu_beta = params["mu_beta"]
     sigma_a = params["sigma_a"]
     sigma_b = params["sigma_b"]
     sigma_beta = params["sigma_beta"]
     sigma_y = params["sigma_y"]
     # initialize transformed parameters
-    y_hat = init_vector("y_hat", dims=(N)) # vector
-    for i in range(1, to_int(N) + 1):
-        y_hat[i - 1] = _pyro_assign(y_hat[i - 1], (((_index_select(a, county[i - 1] - 1)  + (_index_select(x, i - 1)  * _index_select(b, county[i - 1] - 1) )) + (_index_select(beta, 1 - 1)  * _index_select(u, i - 1) )) + (_index_select(beta, 2 - 1)  * _index_select(inter, i - 1) )))
-    # model block
-
-    mu_beta =  _pyro_sample(mu_beta, "mu_beta", "normal", [0., 1])
-    beta =  _pyro_sample(beta, "beta", "normal", [(100 * mu_beta), sigma_beta])
-    mu_a =  _pyro_sample(mu_a, "mu_a", "normal", [0., 1])
-    a =  _pyro_sample(a, "a", "normal", [mu_a, sigma_a])
-    mu_b =  _pyro_sample(mu_b, "mu_b", "normal", [0., 1])
-    b =  _pyro_sample(b, "b", "normal", [(0.10000000000000001 * mu_b), sigma_b])
-    y =  _pyro_sample(y, "y", "normal", [y_hat, sigma_y], obs=y)
+    mu_beta =  pyro.sample("mu_beta", dist.Normal(0., 1.))
+    beta =  pyro.sample("beta", dist.Normal(100 * mu_beta, sigma_beta).expand([2]))
+    mu_a =  pyro.sample("mu_a", dist.Normal(0., 1.))
+    a =  pyro.sample("a", dist.Normal(mu_a, sigma_a).expand([85]))
+    mu_b =  pyro.sample("mu_b", dist.Normal(0., 1.))
+    b =  pyro.sample("b", dist.Normal((0.1 * mu_b), sigma_b).expand([85]))
+    y_hat = a[county] + x * b[county] + beta[0] * u + beta[1] * inter
+    y =  pyro.sample("y", dist.Normal(y_hat, sigma_y), obs=y)
 
