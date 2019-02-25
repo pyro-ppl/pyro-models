6449a31b9493fd1368226a51642bc698e044e5ee
jpchen <jonathanp.chen@gmail.com>
2019-02-25 11:00:24 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import, division, print_function
+
+from setuptools import find_packages, setup
+
+setup(
+    name='Pyro Models',
+    version='0.0.0',
+    description='Models written in Pyro Probabilistic Programming Language',
+    packages=find_packages(include=['pyro']),
+    url='https://github.com/pyro-ppl/pyro-models',
+    author='Uber AI Labs',
+    author_email='jpchen@uber.com',
+    install_requires=[
+        'pyro-ppl>=0.3',
+        'torch>=1.0.0',
+    ],
+    extras_require={
+        'test': ['flake8', 'pytest>=4.1'],
+    },
+    tests_require=['flake8', 'pytest>=4.1'],
+    keywords='probabilistic machine learning bayesian statistics pytorch',
+    license='MIT License',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
