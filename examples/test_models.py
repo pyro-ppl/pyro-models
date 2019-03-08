823e8604de66a1f5c1195c03e95044d5379e5c7c
Stefan Webb <stefan.webb@wadham.ox.ac.uk>
2019-03-07 16:12:28 -0800

    Rearranging model files and removing indexing by number

--- examples/test_models.py
+++ examples/test_models.py
@@ -11,25 +11,13 @@ import pyro.optim as optim
 import pyro_models
 
 def select_model(args, models):
-    if args.model_name is None and args.model_index is None:
-        raise Exception('One of model name or model index must be specified in command arguments!')
-    
-    if args.model_name is not None and args.model_index is not None:
-        raise Exception('Cannot specify both model name and index in command arguments!')
+    # Check that model is specified and exists
+    if args.model_name is not None and args.model_name not in models:
+        raise Exception(f'Model named {args.model_name} is not present in model zoo!')    
+    elif args.model_name is None:
+        raise Exception(f'Model name not specified in command arguments!')
 
-    if args.model_name is not None:
-        those_models = [m for idx, m in models.items() if m['model_name'] == args.model_name]
-        if those_models == []:
-            raise Exception(f'Model named {args.model_name} is not present in model zoo!')
-        elif len(those_models) > 1:
-            raise Exception(f'Model {args.model_name} is not uniquely named in model zoo!')
-        print(those_models)
-        model_dict = those_models[0]
-    else:
-        if args.model_index not in models:
-            raise Exception(f'Model index {args.model_index} is not present in model zoo!')
-        model_dict = models[args.model_index]
-    return model_dict
+    return models[args.model_name]
 
 def main(args):
     # Init Pyro
@@ -54,8 +42,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="parse args")
-    parser.add_argument('-n', '--num-epochs', default=100, type=int)
-    parser.add_argument('-m', '--model-name', type=str, help="model name given by filename")
-    parser.add_argument('-i', '--model-index', type=int, help="index of model given by prefix")
+    parser.add_argument('-n', '--num-epochs', default=100, type=int, help="number of epochs to run learning for")
+    parser.add_argument('-m', '--model-name', type=str, help="model name qualified by dataset")
     args = parser.parse_args()
     main(args)
--- pyro_models/load.py
+++ pyro_models/load.py
@@ -5,8 +5,8 @@ import imp
 from functools import partial
 
 def load():
-    model_dir = pyro_models.__path__[0]
-    model_files = [ f for f in os.listdir(model_dir)  if os.path.isfile(os.path.join(model_dir, f)) and f[0].isdigit() ]
+    base_dir = pyro_models.__path__[0]
+    model_dirs = [ f for f in os.listdir(base_dir)  if not os.path.isfile(os.path.join(base_dir, f)) and not f.startswith('_') ]
     models = {}
 
     def model_wrapped(foo, data, params):
@@ -14,23 +14,33 @@ def load():
         # in Stan's "parameters" block are actually random variables
         foo.model(data, foo.init_params(data))
 
-    for f in model_files:
-        number, filename = f.split('_', 1)
-        model = models.setdefault(int(number), {})
+    for d in model_dirs:
+      model_dir = os.path.join(base_dir, d)
+      # Check that this is a python module
+      if not os.path.isfile(os.path.join(model_dir, '__init__.py')):
+        continue
 
-        if filename.endswith('.py'):
-            model['model_file'] = os.path.join(model_dir, f)
-            model['model_name'] = filename[:-3].replace('_', ' ').lower()
-            model['index'] = number
-            foo = imp.load_source('model.'+filename[:-3].lower(), os.path.join(model_dir, f))
-            model['module'] = foo 
+      model_files = [ f for f in os.listdir(model_dir)  if os.path.isfile(os.path.join(model_dir, f)) and not f.startswith('_') ]
 
-            # NOTE: Need to use partial to make sure correct foo is bound to function!
-            model['model'] = partial(model_wrapped, foo)
+      for f in model_files:
+          # Model name is qualified by source
+          name, ext = f.split('.', 1)
+          fullname = d + '.' + name.lower()
+          model = models.setdefault(fullname, {})
 
-        elif filename.endswith('.py.json'):
-            model['data_file'] = os.path.join(model_dir, f)
-        else:
-            raise Exception(f'Invalid file {f} in model zoo')
+          if f.endswith('.py'):
+              model['dataset'] = d
+              model['source_file'] = os.path.join(model_dir, f)
+              model['name'] = name.lower()
+              foo = imp.load_source('model.'+f[:-3].lower(), os.path.join(model_dir, f))
+              model['module'] = foo 
+
+              # NOTE: Need to use partial to make sure correct foo is bound to function!
+              model['model'] = partial(model_wrapped, foo)
+
+          elif f.endswith('.py.json'):
+              model['data_file'] = os.path.join(model_dir, f)
+          else:
+              raise Exception(f'Invalid file {f} in model zoo')
 
     return models
