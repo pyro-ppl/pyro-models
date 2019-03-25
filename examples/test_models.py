5c24e813e326b08ecfb12bcebf96c5f6d843f545
jpchen <jonathanp.chen@gmail.com>
2019-03-24 17:42:06 -0700

    revert changes to test_models

--- examples/test_models.py
+++ examples/test_models.py
@@ -4,8 +4,8 @@ import sys
 import torch
 import pyro
 import pyro.distributions as dist
-from pyro.infer import SVI, Trace_ELBO, RenyiELBO
-from pyro.contrib.autoguide import AutoDelta, AutoDiagonalNormal, AutoMultivariateNormal
+from pyro.infer import SVI, Trace_ELBO
+from pyro.contrib.autoguide import AutoDelta, AutoDiagonalNormal
 import pyro.optim as optim
 
 import pyro_models
@@ -27,31 +27,22 @@ def main(args):
     # Load meta-data for all models and select model based on command arguments
     models = pyro_models.load()
     model_dict = select_model(args, models)
-    #model_dict = models['arm.radon_inter_vary']
-    #model_dict = models['arm.radon_group']
-    #model_dict = models['arm.wells_dist']
-    #model_dict = models['arm.earnings_latin_square_chr']
-    #model_dict = models['arm.anova_radon_nopred']
 
     # Define model/data/guide
     model = model_dict['model']
     data = pyro_models.data(model_dict)
-    #guide = AutoDiagonalNormal(model)
-    guide = AutoMultivariateNormal(model)
-    #guide = AutoDelta(model)
+    guide = AutoDelta(model)
 
     # Perform variational inference
-    svi = SVI(model, guide, optim.Adam({'lr': 0.1}), loss=Trace_ELBO(vectorize_particles=True, num_particles=10))
-    iwae = RenyiELBO(vectorize_particles=True, num_particles=5000)
-
+    svi = SVI(model, guide, optim.Adam({'lr': 0.1}), loss=Trace_ELBO())
     for i in range(args.num_epochs):
         params = {}
         loss = svi.step(data, params)
-        print('elbo', loss,'| iwae', iwae.loss(model, guide, data, {}))
+        print(loss)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="parse args")
-    parser.add_argument('-n', '--num-epochs', default=1000, type=int, help="number of epochs to run learning for")
+    parser.add_argument('-n', '--num-epochs', default=100, type=int, help="number of epochs to run learning for")
     parser.add_argument('-m', '--model-name', type=str, help="model name qualified by dataset")
     args = parser.parse_args()
     main(args)
